4f710f7dcca8c6faf01cb5b72c7c1a4bef71f5e6
JohnONeillGMIT <36233106+JohnONeillGMIT@users.noreply.github.com>
2019-12-29 00:37:37 +0000

    9.02 Python interaction with Database working OK.
    Set up for my Employee Directory table .. all working correctly

--- mySQLEmplDirectoryDAO.py
+++ mySQLEmplDirectoryDAO.py
@@ -1,20 +1,52 @@
 import mysql.connector
+# creating a Class
+class EmplDirectoryDAO:
+    db=""
+    def __init__(self):
+        self.db = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "root",
+        database ="datarepresentation",
+        auth_plugin="mysql_native_password"
+        )
+    def create(self,values):
+        cursor = self.db.cursor()
+        sql = "insert into Empldirectory (first_name,surname,Ext) values (%s,%s,%s)"
+        cursor.execute(sql,values)
 
-db = mysql.connector.connect(
-    host = "localhost",
-    user= "root",
-    password= "root",
-    database="datarepresentation"
-)
+        self.db.commit()
+        return cursor.lastrowid
 
-cursor = db.cursor()
-sql="delete from empldirectory where id = %s"
-values = (10001,) #be sure to leave in the comma as tuple!
+    def getAll(self):
+        cursor = self.db.cursor()
+        sql ="select * from EmplDirectory"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
 
-cursor.execute(sql,values)
-db.commit()
-print("delete done")
+    def findByID(self,id):
+        cursor = self.db.cursor()
+        sql = "select * from EmplDirectory where id = %s"
+        values = (id,)
 
+        cursor.execute(sql,values)
+        result = cursor.fetchone()
+        return result
 
+    def update(self,values):
+        cursor = self.db.cursor()
+        sql ="update EmplDirectory set first_name= %s,surname=%s,Ext=%s where id= %s"
+        cursor.execute(sql, values)
+        self.db.commit()
 
+    def delete(self,id):
+        cursor = self.db.cursor()
+        sql = "delete from EmplDirectory where id = %s"
+        values = (id,)
 
+        cursor.execute(sql,values)
+        self.db.commit()
+        print("delete done")
+
+emplDirectoryDAO = EmplDirectoryDAO()
